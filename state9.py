9f661e31840c8b698b5d429bee369955dc320087
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-10 12:17:53 +0100

    Add files via upload

---
+++ state9.py
@@ -0,0 +1,15 @@
+import time
+
+def state9_change(state9):
+    state9=state9+1
+    if state9 >8:
+        state9=0
+    return state9     
+
+
+print("\033c\033[40;37m\nquantum emulator of a state9\n")
+states=0
+for a in range (20):
+     print(states)
+     states=state9_change(states)
+     
